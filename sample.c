09ddc7a5afe3b869c45a39341648877bea4b7c18
guye1296 <ezer.guy@gmail.com>
2019-06-04 01:59:47 +0300

    made a bit more l33t

--- sample.c
+++ sample.c
@@ -1,4 +1,11 @@
+#include <stdio.h>
+
 int main(void) {
-	char *foo = "AAAAAAAA";
-	foo[0] = 'B';
+    char *foo = ":(";
+
+    printf("printing string foo %s\n", foo);
+    foo[1] = ')';
+    printf("printing string foo %s\n", foo);
+    
+    return 0;
 }
